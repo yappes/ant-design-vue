c69a360576f2fda5a17174330f0034d8047e67d0
马晨阳 <machenyang@agree.com.cn>
2019-03-06 13:57:46 +0800

    sass依赖，axios依赖service

---
+++ src/page/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        hello word
+    </div>
+</template>
+<script>
+import Service from '@/services/service.js'
+export default {
+    name:'Home',
+    mounted(){
+        console.log(Service.getCountInfo)
+    }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
+
+
---
+++ src/services/service.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+class Service {
+    getCountInfo({}) {
+        return '111111'
+      }
+}
+export default new Service()
